51e93333cc378b6f6f7a2737881559d10f140ce4
samuel-poinama <samuel.poinama@efrei.net>
2024-05-24 12:00:50 +0100

    fix home

--- src/components/panel/home.tsx
+++ src/components/panel/home.tsx
@@ -5,49 +5,243 @@ import Image from 'next/image'
 
 export default function HomePanel() {
     return (
-        <div>  
-            <div className="paire">
-                <div className="pair">
-                    <div className="a">
-                        <Image src="/comunity.png" alt="comunity" width={20} height={100}/>
-                        <div>
-                            <h4>information a rentrer</h4>
-                            <h3>Total Users</h3>
+            <div className="adminhome">
+                <div className="info">
+                    <div className="top">
+    
+                        <div className="total">
+                            <div className="adminhome-form">
+                                <div className="total-forms">
+                                    <div className="title">
+                                        <h2>Total Forms</h2>
+                                    </div>
+                                    <div className="button">
+                                        <a href="#" className="view-link"> VIEW FORM LIST</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="last">
+                            <div className="adminhome-lastforms">
+                                <div className="last-lastforms">
+                                    <div className="title">
+                                        <h2>Last Forms</h2>
+                                    </div>
+                                    <div className="button">
+                                        <ul>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Form name</h2>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Form name</h2>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Form name</h2>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Form name</h2>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Form name</h2>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <a href="*">View All </a>
+                    <div className="bottom">
+                        <div className="total">
+                        <div className="adminhome-user">
+                            <div className="iframe total-forms">
+                                <div className="title">
+                                    <h2>Total Users</h2>
+                                </div>
+                                <div className="button">
+                                    <a href="#" className="view-link"> VIEW USER LIST</a>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="last">
+                            <div className="adminhome-members">
+                                <div className="iframe last-members">
+                                    <div className="title">
+                                        <h2>Last Members</h2>
+                                    </div>
+                                    <div className="button">
+                                        <ul>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Name</h2>
+                                                    <h3>role</h3>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Name</h2>
+                                                    <h3>role</h3>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Name</h2>
+                                                    <h3>role</h3>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Name</h2>
+                                                    <h3>role</h3>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Name</h2>
+                                                    <h3>role</h3>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Name</h2>
+                                                    <h3>role</h3>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Name</h2>
+                                                    <h3>role</h3>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Name</h2>
+                                                    <h3>role</h3>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Name</h2>
+                                                    <h3>role</h3>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Name</h2>
+                                                    <h3>role</h3>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Name</h2>
+                                                    <h3>role</h3>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                            <li>
+                                                <Image src="/ntc2.png" alt="home" width={50} height={50} />
+                                                <div className="text">
+                                                    <h2>Name</h2>
+                                                    <h3>role</h3>
+                                                </div>
+                                                <button className="set">
+                                                    <Image src="/settings.png" alt="home" width={20} height={20} />
+                                                </button>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            
-                <div className="pair">
-                    <div className="a">
-                        <Image src="/forms.png" alt="forms" width={20} height={100}/>
-                        <div>
-                            <h4>information a rentrer</h4>
-                            <h3>Total Users</h3>
+    
+    
+                <div className="deco">
+    
+    
+                    <div className="adminhome-deco">
+                        <div className="deco">
+                            <h2>SKILL, EXPERIENCES, AND FRIENDS FOR LIFE.</h2>
+                            <Image src="/ntc2.png" alt="home" width={200} height={200} />
                         </div>
                     </div>
-                    <a href="*">View All </a>
-                </div>
-            
-            
-                <div className ="pair">
-                    <div className="a">
-                        <Image src="/navigation.png" alt="" width={20} height={100}/>
-                        <div>
-                            <h4>information a rentrer</h4>
-                            <h3>Total Users</h3>
-                        </div>
-                    </div>
-                    <a href="*">View All </a>
+    
+    
                 </div>
             </div>
-
-            <div className="single">
-                <div>
-                    <div className="a"/>
-                    <a href="*">View All </a>
-                </div>
-            </div>
-        </div>
-    )
+        )
 }
