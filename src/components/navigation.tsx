b4b32c56aed4e6144f50961d1de8cb6956dd87c0
samuel-poinama <samuel.poinama@efrei.net>
2024-05-24 12:46:25 +0100

    fix nav bar

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -4,26 +4,38 @@ import Link from "next/link"
 
 
 export default function Navigation() {
+
     return (
-    <div className="navigation">
-        <Link className="nav_button" href="/panel/home" >
+        <div className="navigation">
+            <Link className="logo" href="/panel/home" >
+                <Image src="/ntc2.png" alt="home" width={80} height={80} />
+
+                <div className="title_logo">
+                    <h1>NTC</h1>
+                    <h3>Admin Panel</h3>
+                </div>
+
+            </Link>
+
+            <Link className="general" href="/panel/home" >
                 <Image src="/home.png" alt="home" width={20} height={100} />
                 <p>General</p>
-        </Link>
+            </Link>
 
-        <Link className="nav_button" href="/panel/users" >
-        <Image src="/users.png" alt="users" width={20} height={100} />
-        <p>Users</p>
-        </Link>
+            <Link className="button" href="/panel/users" >
+                <Image src="/users.png" alt="users" width={20} height={100} />
+                <p>Users</p>
+            </Link>
 
-        <Link className="nav_button" href="/panel/forms" >
-        <Image src="/forms.png" alt="forms" width={20} height={100} />
-        <p>Forms</p>
-        </Link>
+            <Link className="button" href="/panel/forms" >
+                <Image src="/forms.png" alt="forms" width={20} height={100} />
+                <p>Forms</p>
+            </Link>
 
-        <Link type="submit" className="sign_out nav_button" href="/logout" >
-        <Image src="/sign out.png" alt="sign out" width={20} height={100} /><p>Sign Out</p>
-        </Link>
-    </div>
+            <Link className="sign_out" href="/logout" >
+                <Image src="/sign out.png" alt="sign out" width={20} height={100} />
+                <p>Sign Out</p>
+            </Link>
+        </div>
     )
 }
