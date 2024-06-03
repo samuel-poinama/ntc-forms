3fab1f1dc39b0dbb639e1f0c86a54f1681100961
samuel-poinama <samuel.poinama@efrei.net>
2024-06-03 17:35:10 +0100

    add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+
+export default function custom404() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-[#2f5597] text-white">
+      <div className="container text-center overflow-hidden">
+        <Image
+          src="/image6.png"
+          alt=""
+          className="absolute top-[15%] left-[56%] h-[15px]"
+          width={15}
+          height={15}
+        />
+        <Image
+          src="/image5.png"
+          alt=""
+          className="absolute top-[30%] left-[60%] h-[100px]"
+          width={100}
+          height={100}
+        />
+        <Image
+          src="/image4.png"
+          alt=""
+          className="absolute top-[40%] left-[43.5%] h-[60px]"
+          width={60}
+          height={60}
+        />
+        <div className="max-h-[40%]">
+          <h1 className="text-[320px] m-0">404</h1>
+          <h2 className="text-[60px] m-0">
+            Oops, This Page Could Not Be Found
+          </h2>
+          <h3 className="text-[16px] m-0">
+            The page you are looking for might have been removed, had its name
+            changed, or is temporarily unavailable.
+          </h3>
+        </div>
+        <Image
+          src="/image3.png"
+          alt=""
+          className="absolute top-[45%] left-[22%] h-[175px]"
+          width={175}
+          height={175}
+        />
+        <Image
+          src="/image2.png"
+          alt=""
+          className="absolute top-[82%] left-[27%] h-[250px]"
+          width={251}
+          height={251}
+        />
+        <div className="mt-[100px]">
+          <button className="bg-[#f4dd13] text-white py-[30px] px-[80px] rounded-[40px] text-[18px] cursor-pointer">
+            Back to Home
+          </button>
+        </div>
+        <Image
+          src="/image1.png"
+          alt=""
+          className="absolute top-[65%] right-0 h-[700px]"
+          width={700}
+          height={700}
+        />
+      </div>
+    </div>
+  );
+}
